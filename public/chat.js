cb069b0627d19de5e04a40736df52594c5027203
Puyan Wei <puyan.wei@mountainwarehouse.com>
2018-08-31 14:50:25 +0100

    Added feedback message for when other user is typing

--- public/chat.js
+++ public/chat.js
@@ -8,6 +8,7 @@ let message = document.getElementById('message');
 let handle = document.getElementById('handle');
 let button = document.getElementById('send');
 let output = document.getElementById('output');
+let feedback = document.getElementById('feedback');
 
 // Emit events
 
@@ -23,15 +24,23 @@ button.addEventListener('click', () => {
 
 message.addEventListener('keyup', event => {
     if (event.keyCode === 13) {
-        console.log(324234);
         button.click();
     }
+});
+
+message.addEventListener('keypress', () => {
+    socketFE.emit('typing', handle.value);
 });
 
 // Listen for events
 
 socketFE.on('chat', data => {
+    feedback.innerHTML = '';
     output.innerHTML += `<p><strong>${data.handle}   </strong>${
         data.message
     }</p>`;
+});
+
+socketFE.on('typing', data => {
+    feedback.innerHTML = `<p><em>${data} is typing a message...</em></p>`;
 });
