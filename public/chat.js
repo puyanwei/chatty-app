680b636d1fc9628376130b665b3220f14abe2c5a
Puyan Wei <puyan.wei@mountainwarehouse.com>
2018-08-31 14:28:07 +0100

    Added functionality for using Enter key to submit message

--- public/chat.js
+++ public/chat.js
@@ -19,6 +19,13 @@ button.addEventListener('click', () => {
     message.value = '';
 });
 
+message.addEventListener('keyup', event => {
+    if (event.keyCode === 13) {
+        console.log(324234);
+        button.click();
+    }
+});
+
 // Listen for events
 
 socketFE.on('chat', data => {
